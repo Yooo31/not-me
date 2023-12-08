7829d8140be6ead0fc912614c62d2f0b59c72acd
Yooo31 <mrts.yoan@gmail.com>
2023-12-08 13:43:40 +0100

    Styling setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
+import clsx from "clsx";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <button type="button" className="text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Pink to Orange</button>
+      <button type="button" className={clsx(
+        "text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+      )}
+      >
+        Pink to Orange
+      </button>
     </main>
   )
 }

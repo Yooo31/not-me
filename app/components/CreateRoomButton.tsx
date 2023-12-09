48d77b04edebf40911e7c06fbb0198801dcfd5f3
Yooo31 <mrts.yoan@gmail.com>
2023-12-09 01:41:04 +0100

    First step of websocket

--- app/components/CreateRoomButton.tsx
+++ app/components/CreateRoomButton.tsx
@@ -1,14 +1,38 @@
 // app/components/CreateRoomButton.tsx
 
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { generateRandomCode } from '@/app/utils/codeGenerator';
-
+import { useRouter } from 'next/navigation';
+import { io } from 'socket.io-client';
 
 const CreateRoomButton: React.FC = () => {
+  const router = useRouter();
+  const [username, setUsername] = useState('');
+  const [roomCode, setRoomCode] = useState('');
+
   const handleCreateRoom = () => {
-    const generatedCode = generateRandomCode(); // Utilise une fonction pour générer un code aléatoire
-    console.log(`/party?code=${generatedCode}`);
+    const generatedCode = generateRandomCode();
+    setRoomCode(generatedCode);
   };
+
+  useEffect(() => {
+    if (roomCode) {
+      const user = prompt('Entrez votre nom d\'utilisateur :');
+      if (user) {
+        setUsername(user);
+        const socket = io();
+
+        socket.on('connect', () => {
+          socket.emit('createRoom', roomCode, user);
+          router.push(`/party?code=${roomCode}`);
+        });
+
+        return () => {
+          socket.disconnect();
+        };
+      }
+    }
+  }, [roomCode, router]);
 
   return (
     <div>
